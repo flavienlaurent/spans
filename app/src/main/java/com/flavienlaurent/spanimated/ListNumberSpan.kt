4000e0a3b90bbc2459cdca692d18c055bef5cd43
赵佳亮 <zhaojialiang@sogou-inc.com>
2020-05-24 18:10:40 +0800

    1. list number background use code instead picture to draw

--- app/src/main/java/com/flavienlaurent/spanimated/ListNumberSpan.kt
+++ app/src/main/java/com/flavienlaurent/spanimated/ListNumberSpan.kt
@@ -22,19 +22,9 @@ class ListNumberSpan(var isCornerGray: Boolean, var cornerText:String =  "",
     private var mWidth = -1
     private val mRectF = RectF()
 
-    private var cornerDrawable: Drawable?
-    private var mCornerBound: Rect = Rect(0,0, 0,0)
-    private var cornerHeight: Int
-    private var cornerWidth: Int
 
     init {
         initPaint()
-        cornerWidth = SpanDimensionUtils.dp2px(context, 17f)
-        cornerHeight = SpanDimensionUtils.dp2px(context, 16f)
-
-        val drawableRes = if(isCornerGray) R.drawable.default_corner_back_one_digital else R.drawable.corner_back_one_digital
-        cornerDrawable = context.getDrawable(drawableRes)
-        updateBackgroundBound(cornerWidth, cornerHeight)
     }
 
 
@@ -50,33 +40,20 @@ class ListNumberSpan(var isCornerGray: Boolean, var cornerText:String =  "",
 
     override fun getSize(paint: Paint, text: CharSequence, start: Int, end: Int, fm: FontMetricsInt?): Int { //return text with relative to the Paint
         mWidth = paint.measureText(text, start, end).toInt()
-
-        cornerWidth = mWidth
-        updateBackgroundBound(cornerWidth, cornerHeight)
-
         return mWidth
-    }
-
-    private fun updateBackgroundBound(right: Int, bottom: Int) {
-        mCornerBound.right = right
-        mCornerBound.bottom = bottom
-        cornerDrawable?.bounds = mCornerBound
     }
 
     override fun draw(canvas: Canvas, text: CharSequence, start: Int, end: Int, x: Float, top: Int, y: Int, bottom: Int, paint: Paint) {
         val charWidth = paint.measureText(cornerText)
-//        mRectF.set(x, top.toFloat(), x + charWidth, bottom.toFloat())
-//        canvas.drawOval(mRectF, mBackgroundPaint)
-
 
         canvas.save()
-        canvas.translate(x, top.toFloat() + cornerHeight/4f)
-        cornerDrawable?.draw(canvas)
+        canvas.translate(0f , (bottom-top) / 8f )
+        mRectF.set(x, top.toFloat(), x + charWidth, bottom.toFloat())
+        canvas.drawRoundRect(mRectF, mRectF.height() / 2f, mRectF.height() / 2f, mBackgroundPaint)
         canvas.restore()
 
-
         canvas.save()
-        canvas.translate(cornerWidth / 2f - 2, 0f)
+        canvas.translate(mWidth / 2f , 0f)
         canvas.drawText(cornerText, 0, cornerText.length, x, y.toFloat(), cornerTextPaint)
         canvas.restore()
 
